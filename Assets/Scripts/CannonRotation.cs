df0c15281f562350b829e2dcdc1bda010bd87ea1
VastoGrim <65916224+VastoGrim@users.noreply.github.com>
2021-08-10 12:33:48 -0500

    Rotación de Cañon
    el hijo "Cannons" ya se rota con Q o E en 90 grados. Se sigue disparando desde el mismo punto, shootPoint esta fuera de Cannons en la jerarquia.

--- Assets/Scripts/CannonRotation.cs
+++ Assets/Scripts/CannonRotation.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 
 public class CannonRotation : MonoBehaviour
 {
+    public GameObject cannons;
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            cannons.transform.Rotate(new Vector3(0, 0, -90));
+        }
+ 
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            cannons.transform.Rotate(new Vector3(0, 0, 90));
+        }
     }
 }
